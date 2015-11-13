85a8c2fe6c096d78590cea5e559d7751e001e447
romain lebrun <lebrun.romain@gmail.com>
2015-11-13 23:58:05 +0100

    no message

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'kawaweb');
+define('DB_NAME', 'wp-kawaweb');
 
 /** Utilisateur de la base de données MySQL. */
 define('DB_USER', 'root');
--- wp-content/themes/kawaweb/header.php
+++ wp-content/themes/kawaweb/header.php
@@ -24,7 +24,7 @@
         <title><?php wp_title( '|', true, 'right' ); ?></title>
         <link rel="profile" href="http://gmpg.org/xfn/11">
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-        <link href='https://fonts.googleapis.com/css?family=Raleway:400,300,700,900' rel='stylesheet' type='text/css'>
+        <link href='<?php echo get_template_directory_uri(); ?>/sass/montserrat/stylesheet.css' rel='stylesheet' type='text/css'>
         <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 <![endif]-->
@@ -34,22 +34,14 @@
     <body <?php body_class(); ?> >
         <div id="page" class="hfeed site">
             <header id="header" >
-                <!--<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-</a>
-
-<div id="navbar" class="navbar">
-<nav id="site-navigation" class="navigation main-navigation" role="navigation">
-
-<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
-<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ) ); ?>
-</nav>
-</div>-->
-                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" id="logo"><img src="<?php echo get_bloginfo('template_directory');?>/images/logo.png" alt=""></a>
-                <nav id="main-nav">
-                    <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ) ); ?>
-                </nav>
+                <div id="top-menu">
+                     <a href="<?php echo esc_url( home_url( '/' ) ); ?>" id="logo"><img src="<?php echo get_bloginfo('template_directory');?>/images/logo-kawaweb.png" alt=""></a>
+                    <nav id="main-nav">
+                        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ) ); ?>
+                    </nav>
+                </div>
+                <div id="head-artwork"></div>
+               
             </header><!-- #masthead -->
 
             <div id="main" class="site-main">
